505710670035b2818aa07213bba6b378bad3595b
Can Ivit <canivit@users.noreply.github.com>
2022-12-01 23:40:02 -0500

    day01 part2

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -8,7 +8,13 @@ pub fn process_part1(input: &str) -> String {
 }
 
 pub fn process_part2(input: &str) -> String {
-    input.to_uppercase()
+    input
+        .split("\n\n")
+        .map(sum_group)
+        .fold([0, 0, 0], update_top_three)
+        .iter()
+        .sum::<u32>()
+        .to_string()
 }
 
 fn sum_group(group: &str) -> u32 {
@@ -17,6 +23,14 @@ fn sum_group(group: &str) -> u32 {
         .map(|calorie| calorie.trim().parse::<u32>().unwrap())
         .sum::<u32>()
 }
+
+fn update_top_three(top_three: [u32; 3], next: u32) -> [u32; 3] {
+    let mut vec = top_three.to_vec();
+    vec.push(next);
+    vec.sort_by(|a, b| b.cmp(a));
+    [vec[0], vec[1], vec[2]]
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -44,6 +58,27 @@ mod tests {
     }
 
     #[test]
+    fn test_process_part2() {
+        let input = "1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000";
+
+        let result = process_part2(input);
+        assert_eq!("45000", result);
+    }
+
+    #[test]
     fn test_sum_group() {
         let group = "125";
         let result = sum_group(group);
@@ -59,9 +94,17 @@ mod tests {
     }
 
     #[test]
-    fn test_process_part2() {
-        let input = "input";
-        let result = process_part2(input);
-        assert_eq!("INPUT", result);
+    fn test_update_top_three() {
+        let top_three = [0, 0, 0];
+        let new_top_three = [2, 0, 0];
+        assert_eq!(new_top_three, update_top_three(top_three, 2));
+
+        let top_three = [8, 5, 3];
+        let new_top_three = [9, 8, 5];
+        assert_eq!(new_top_three, update_top_three(top_three, 9));
+
+        let top_three = [8, 5, 3];
+        assert_eq!(top_three, update_top_three(top_three, 2));
     }
+    
 }
