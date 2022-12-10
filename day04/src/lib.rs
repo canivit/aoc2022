3a62ffde86acc4675696e4fdf34c53a91f9aed6e
Can Ivit <canivit@users.noreply.github.com>
2022-12-09 23:08:33 -0500

    day04 part2

--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -6,8 +6,12 @@ pub fn process_part1(input: &str) -> usize {
         .count()
 }
 
-pub fn process_part2(input: &str) -> String {
-    input.to_uppercase()
+pub fn process_part2(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| line_to_range_pair(line.trim()))
+        .filter(|pair| pair.0.does_overlap(&pair.1))
+        .count()
 }
 
 #[derive(PartialEq, Debug)]
@@ -23,6 +27,11 @@ impl Range {
 
     fn does_one_fully_contain_other(&self, other: &Range) -> bool {
         self.does_fully_contain(other) || other.does_fully_contain(self)
+    }
+
+    fn does_overlap(&self, other: &Range) -> bool {
+        (self.start >= other.start && self.start <= other.end)
+            || (other.start >= self.start && other.start <= self.end)
     }
 }
 
@@ -56,6 +65,32 @@ mod tests {
     }
 
     #[test]
+    fn test_process_part2() {
+        let input = "2-4,6-8
+            2-3,4-5
+            5-7,7-9
+            2-8,3-7
+            6-6,4-6
+            2-6,4-8";
+        let result = process_part2(input);
+        assert_eq!(4, result);
+    }
+
+    #[test]
+    fn test_does_overlap() {
+        let range1 = Range { start: 3, end: 7 };
+        let range2 = Range { start: 5, end: 8 };
+        let range3 = Range { start: 0, end: 4 };
+        let range4 = Range { start: 4, end: 6 };
+        assert!(range1.does_overlap(&range2));
+        assert!(range2.does_overlap(&range1));
+        assert!(range1.does_overlap(&range4));
+        assert!(range4.does_overlap(&range1));
+        assert!(!range2.does_overlap(&range3));
+        assert!(!range3.does_overlap(&range2));
+    }
+
+    #[test]
     fn test_does_one_fully_contain_other() {
         let range1 = Range { start: 1, end: 7 };
         let range2 = Range { start: 3, end: 6 };
@@ -76,12 +111,5 @@ mod tests {
     #[test]
     fn test_line_part_to_range() {
         assert_eq!(Range { start: 2, end: 6 }, line_part_to_range("2-6"));
-    }
-
-    #[test]
-    fn test_process_part2() {
-        let input = "input";
-        let result = process_part2(input);
-        assert_eq!("INPUT", result);
     }
 }
